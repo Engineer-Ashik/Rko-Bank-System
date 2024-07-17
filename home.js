575726dc14fce2f7dc653f954f4aa27208c75c6c
Ashik <engineerashikur@gmail.com>
2024-07-17 12:39:12 +0600

    Adding the HomePage login system

--- home.js
+++ home.js
@@ -1,1 +1,17 @@
-console.log('connected js');
+console.log('Hello RKO');
+document.getElementById('submit').addEventListener('click', function loginpass(){
+    const loginid = document.getElementById('login');
+    const loginvalue = loginid.value;
+
+    const passwordid = document.getElementById('password');
+    const passwordvalue = passwordid.value;
+
+    if(loginvalue === 'web@yahoo.com' && passwordvalue === '1234'){
+        window.location.href = 'service.html';
+    }
+
+    else{
+        alert('Try Again.. Wrong Email or Password');
+    }
+})
+
